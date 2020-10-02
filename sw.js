7cabb3ca4faa765d6e04820160fcf3694527aa4f
L-shuai <1472174772@qq.com>
2020-10-02 20:47:42 +0800

    Site updated: 2020-10-02 20:47:35

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/49821/','/posts/16107/','/posts/50222/','/posts/4759/','/posts/3164/','/posts/6004/','/posts/2915/','/posts/46208/','/posts/49651/','/posts/8716/','/posts/4785/','/posts/64366/','/posts/26104/','/posts/2141/','/posts/51385/','/posts/12019/','/posts/218/','/posts/64322/','/posts/9628/','/about/index.html','/icons/index.html','/gallery/index.html','/letter/index.html','/library/index.html','/records/content.json','/records/index.html','/resume/index.html',
+'/','/posts/14783/','/posts/50220/','/posts/49821/','/posts/16107/','/posts/50222/','/posts/4759/','/posts/3164/','/posts/6004/','/posts/2915/','/posts/46208/','/posts/49651/','/posts/8716/','/posts/4785/','/posts/64366/','/posts/26104/','/posts/2141/','/posts/51385/','/posts/12019/','/posts/218/','/posts/64322/','/posts/9628/','/about/index.html','/icons/index.html','/gallery/index.html','/letter/index.html','/records/index.html','/records/content.json','/resume/index.html','/library/index.html',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
